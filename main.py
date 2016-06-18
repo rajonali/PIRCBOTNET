17459e5d63848e77e3cd95f793dbab51b2f49777
Rajon Ali <rajonali44492@gmail.com>
2016-06-18 17:45:28 -0500

    Added more functionality to main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import socket
+import sqlite3
 
 
 class irc():
@@ -13,6 +14,8 @@ class irc():
     irc_port = None
     irc_cmdchannel = None
     irc_botchannel = None
+    users_db = None    
+
     def __init__(self):
         try:
             with open(self.config, 'r') as f:
@@ -26,24 +29,48 @@ class irc():
         self.motd = self.config["motd-location"]
         banner_buffer = []
         banner = open(self.banner)
-        motd = open(self.motd, 'w')
+        try:
+            motd = open(self.motd, 'w')
+        except Exception as e:
+            print(e ,"Try running with super user perms.")
         for line in banner:
             banner_buffer.append(line)
         motd.write(str("".join(banner_buffer))  + " Version:: %s " %str(self.version)  + "\n" + " Author(s):: %s" %str(", ".join(self.authors)) +  "\n" + (" "+"="*42) + "\n")
 
     def connect_server(self):
-	pass
+        pass
     def join_channel(self):
-	pass
+        pass
     def send_command(self):
+        pass
+    def give_perms(self):
 	pass
-    def 
-
 
 
 class conn_db():
-    pass
+    cursor = None
+    def __init__(self, db, *args, **kwargs):
+	self.database = db
+        print("Connecting to database...")
+	try:
+            self.conn = sqlite3.connect(str(self.database), *args, **kwargs)
+            print("SUCCESSFULLY CONNECTED TO DATABASE.")
+	    self.cursor = self.conn.cursor()
+	    return self.cursor
+	except Exception as e:
+	    print("UNABLE TO CONNECT TO DATABASE. Trying again...")
+	    sleep(1)
+	    self.connect_db(self,db,*args, **kwargs)
 
+    def execute(self, command):
+        return self.users_db.execute(str(command))
+
+    def commit(self):
+        connec
+    def close_db(self):
+	return self.users_db.close()
+
+ 
 class plugin():
     pass
 
